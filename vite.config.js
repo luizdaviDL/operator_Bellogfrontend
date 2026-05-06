3e5d9496bef4ef0a663922706d72a8598cf7e2ce
Luiz Davi <luizdaviamorim.com@gmail.com>
2026-05-05 21:28:33 -0300

    updated to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // <- isso aqui é necessário para que ao dar refresh não cause erro 404 e seja redirecionado ao index.html e deixar o react router cuidar de tudo
+  }
 })
